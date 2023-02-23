5dcabd7f145e5bd03235b82f7f94f45651042aec
Chidiebere Ibiam <86835158+ChidiebereIbiam@users.noreply.github.com>
2023-02-23 10:37:05 +0100

    Made API more RESTFUL and added POST request

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,16 +14,27 @@ def endpoints(request):
     data = ['/advocates', 'advocates/:username']
     return Response(data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def advocates_list(request):
-    query = request.GET.get('query')
+    #Handles Get requests
+    if request.method == "GET":
+        query = request.GET.get('query')
 
-    if query == None:
-        query = ''
+        if query == None:
+            query = ''
 
-    advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
-    serializer = AdvocateSerializer(advocates, many=True)
-    return Response(serializer.data)
+        advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
+        serializer = AdvocateSerializer(advocates, many=True)
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        advocate = Advocate.objects.create(
+            username = request.data['username'],
+            bio = request.data['bio']
+        )
+        serializer = AdvocateSerializer(advocate, many = False)
+
+        return Response(serializer.data)
 
 @api_view(['GET'])
 def advocate_detail(request, username):
